cfa647465f509d3e762fa471e059f82223cbd269
Егорка <egorkaevd210@gmail.com>
2024-10-16 12:39:36 +0300

    dev: home page updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Catalog from "../Components/Catalog";
+import grape from "../grape.png";
+import "./Home.css";
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <div className='main'>
-            <div>
-              <h1>Profit</h1>
+      <div className="container-home">
+        <div className="inner-container-home">
+          <div className="catalog">
+            <Catalog />
+          </div>
+          <div className="content-container">
+            <div className="stories">
+              <div className="white"></div>
+              <div className="white"></div>
+              <div className="white"></div>
             </div>
+            <div className="content">
+              <h1>
+                Авторизуйтесь! <br /> И получите полный доступ к функциям.
+              </h1>
+              <img src={grape} />
+            </div>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
